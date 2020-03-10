9d867126c5b0a98ee7f17bd8b6e7b38df7371e6e
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-03-10 11:11:04 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
+
+
+  function addTask(task) {
+      let newTask = document.createElement("li");
+      newTask.textContent = task;
+      document.querySelector("ul").appendChild(newTask);
+      }
+      
+
+
+      document.addEventListener("DOMContentLoaded", () => {
+
+        const form = 
+          document.getElementById("create-task-form");
+          form.addEventListener("submit",
+        function(event) {
+          event.preventDefault();
+        const taskToAdd = event.target["new-task-description"].value;
+          if (taskToAdd !== "") {
+        addTask(taskToAdd);
+        }
+        }); 
+
+
+
+  //function addTasks(task) {
+   // let newTask = document.createElement("li");
+  //  newTask.textContent = task;
+  //  document.querySelector("ul").appendChild(newTask);
+
+ // const form = document.getElementById("create-task-form");
+
+
+  //Event.preventDefault
+//});
+
+//document.addEventListener("click", () => {
   // your code here
-});
+ // function 
+//}
